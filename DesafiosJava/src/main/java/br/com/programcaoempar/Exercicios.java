f8d5cbcbe601c057d85083c44588c09e06e43eb5
Tiago <tiagocasa@gmail.com>
2022-08-01 14:51:52 -0300

    Exercicio 17

--- DesafiosJava/src/main/java/br/com/programcaoempar/Exercicios.java
+++ DesafiosJava/src/main/java/br/com/programcaoempar/Exercicios.java
@@ -1,5 +1,7 @@
 package br.com.programcaoempar;
 
+import java.util.*;
+
 public class Exercicios {
     public Integer areaRetangulo(int base, int altura) {
         return base*altura;
@@ -13,13 +15,136 @@ public class Exercicios {
     public int perimetroQuadrado(int base, int altura) {
         return (base*2)+(altura*2);
     }
-
-
     public double areaCirculo(int raio) {
         return Math.PI*Math.pow(raio,2);
     }
-
     public double perimetroCirculo(int raio) {
         return 2*Math.PI*raio;
+    }
+
+    public int perimetroTriangulo(int ladoa, int ladob, int ladoc) {
+        return ladoa+ladob+ladoc;
+    }
+
+    public int sucessor(int numero) {
+        numero++;
+        return numero;
+    }
+
+    public int quociente(int numeroa, int numerob) {
+        return numeroa/numerob;
+    }
+
+    public int restoDivisao(int numeroa, int numerob){
+        return numeroa%numerob;
+    }
+
+    public int idadeAnos(int idade) {
+        return idade/365;
+    }
+    public int idadeMeses(int idade) {
+        return (idade%365)/30;
+    }
+    public int idadeDias(int idade) {
+        return (idade%365)%30;
+    }
+
+    public int fahrenheitToCelsius(int tempFahr) {
+        return (5/9)*(tempFahr-32);
+    }
+
+    public double volumeLata(int raio, int altura) {
+        return areaCirculo(raio) * altura;
+    }
+
+    public int convertToBinary(int inteiro) {
+        ArrayList<Integer> binario = new ArrayList<Integer>();
+        String binarioFinal = "";
+        int i=0;
+        while (inteiro>0){
+            if(inteiro%2 == 0){
+                binario.add(0);
+            }else{
+                binario.add(1);
+            }
+            inteiro /= 2;
+            i++;
+        }
+        for (i= binario.size()-1;i>=0;i--){
+            binarioFinal = binarioFinal+ binario.get(i).toString();
+        }
+        return Integer.parseInt(binarioFinal);
+    }
+
+
+    public int notaSemestral(int provaUmPriBi, int provaDoisPriBi, int provaUmSegBi, int provaDoisSegBi) {
+        int notaPriBi = (provaUmPriBi + provaDoisPriBi)/2;
+        int notaSegBi = (provaUmSegBi + provaDoisSegBi)/2;
+        return (notaPriBi+notaSegBi)/2;
+    }
+
+    public float velocidadeToKMH(float velocidadeMPS) {
+        return velocidadeMPS*3.6f;
+    }
+
+    public float circuitoEletrico(float r1, float r2, float r3) {
+        return (r1*r2/(r1+r2))+r3;
+    }
+
+    public int tempoSemafaro(int acelaracao, int distancia, int velocidadeMax) {
+        int tempoVelocidadeMax = velocidadeMax/acelaracao;
+        int tempoDistancia = distancia/velocidadeMax;
+        return tempoVelocidadeMax+tempoDistancia;
+    }
+
+    public int alturaPredio(int sombraPredio, int sombraPessoa, int alturaPessoa) {
+        return sombraPredio*alturaPessoa/sombraPessoa;
+    }
+
+    public int inverterNumero(int numero) {
+        int novoNum = 0;
+        while (numero > 0){
+            novoNum = novoNum * 10+numero%10;
+            numero /= 10;
+        }
+        return novoNum;
+    }
+
+    public String saqueOtimo(int saque) {
+        ArrayList<Integer> notas = new ArrayList<>(Arrays.asList(100,50,20,10,5,2,1));
+        ArrayList<Integer> notasSaque = new ArrayList<>();
+
+        while (saque>0){
+            if(saque>notas(0))
+        }
+
+        if(saque>=100){
+            notas100 = saque/100;
+            saque%=100;
+        }
+        if(saque>=50){
+            notas50 = saque/50;
+            saque%=50;
+        }
+        if(saque>=20){
+            notas20 = saque/20;
+            saque%=20;
+        }
+        if(saque>=10){
+            notas10 = saque/10;
+            saque%=10;
+        }
+        if(saque>=5){
+            notas5 = saque/5;
+            saque%=5;
+        }
+        if(saque>=2){
+            notas2 = saque/2;
+            saque%=2;
+        }
+        if(saque==1){
+            notas1 = saque;
+        }
+        return "(100:"+notas100+",50:"+notas50+",20:"+notas20+",10:"+notas10+",5:"+notas5+",2:"+notas2+",1:"+notas1+")";
     }
 }
--- DesafiosJava/src/test/java/br/com/programcaoempar/Testes.java
+++ DesafiosJava/src/test/java/br/com/programcaoempar/Testes.java
@@ -10,26 +10,123 @@ public class Testes {
     Exercicios exe;
     @BeforeEach
     void inicializacao(){ exe = new Exercicios();}
-@Test
-void testeExercicio1(){
-        int base = 4;
+    @Test
+    void testeExercicio1(){
+            int base = 4;
+            int altura = 5;
+        assertEquals(20,exe.areaRetangulo(base,altura));
+        assertEquals(18,exe.perimetroRetangulo(base,altura));
+    }
+    @Test
+    void testeExercicio2(){
+        int base = 5;
         int altura = 5;
-    assertEquals(20,exe.areaRetangulo(base,altura));
-    assertEquals(18,exe.perimetroRetangulo(base,altura));
-}
-@Test
-void testeExercicio2(){
-    int base = 5;
-    int altura = 5;
-    assertEquals(20,exe.areaQuadrado(base,altura));
-    assertEquals(18,exe.perimetroQuadrado(base,altura));
-}
+        assertEquals(25,exe.areaQuadrado(base,altura));
+        assertEquals(20,exe.perimetroQuadrado(base,altura));
+    }
 
-@Test
-    void testeExercicio3(){
-        int raio = 2;
-        assertEquals(12.566370614359172,exe.areaCirculo(raio));
-        assertEquals(12.566370614359172,exe.perimetroCirculo(raio));
-}
+    @Test
+        void testeExercicio3(){
+            int raio = 2;
+            assertEquals(12.566370614359172,exe.areaCirculo(raio));
+            assertEquals(12.566370614359172,exe.perimetroCirculo(raio));
+    }
+    @Test
+    void testeExercicio4(){
+        int ladoa = 2;
+        int ladob = 3;
+        int ladoc = 5;
+        assertEquals(10,exe.perimetroTriangulo(ladoa,ladob,ladoc));
+    }
+    @Test
+    void testeExercicio5(){
+        int numero = 25;
+        assertEquals(26,exe.sucessor(numero));
+    }
+    @Test
+    void testeExercicio6(){
+        int numeroa = 26;
+        int numerob = 5;
+        assertEquals(5,exe.quociente(numeroa,numerob));
+        assertEquals(1,exe.restoDivisao(numeroa,numerob));
+    }
+
+    @Test
+    void testeExercicio7(){
+        int idade = 9256;
+        assertEquals(25, exe.idadeAnos(idade));
+        assertEquals(4, exe.idadeMeses(idade));
+        assertEquals(11, exe.idadeDias(idade));
+    }
+
+    @Test
+    void testeExercicio8(){
+        int tempFahr = 100;
+        assertEquals(0, exe.fahrenheitToCelsius(tempFahr));
+    }
+
+    @Test
+    void testeExercicio9(){
+        int raio = 5;
+        int altura = 15;
+        assertEquals(1178.0972450961724, exe.volumeLata(raio,altura));
+    }
+
+    @Test
+    void testeExercicio10(){
+        int inteiro = 30;
+        assertEquals(11110,exe.convertToBinary(inteiro));
+    }
+
+    @Test
+    void testeExercicio11(){
+        int provaUmPriBi = 4;
+        int provaDoisPriBi = 10;
+        int provaUmSegBi = 6;
+        int provaDoisSegBi = 8;
+        assertEquals(7,exe.notaSemestral(provaUmPriBi,provaDoisPriBi,provaUmSegBi,provaDoisSegBi));
+    }
+
+    @Test
+    void testeExercicio12(){
+        float velocidadeMPS = 15;
+        assertEquals(54,exe.velocidadeToKMH(velocidadeMPS));
+    }
+
+    @Test
+    void testeExercicio13(){
+        float r1 = 10;
+        float r2 = 30;
+        float r3 = 5;
+        assertEquals(12.5,exe.circuitoEletrico(r1,r2,r3));
+    }
+
+    @Test
+    void testeExercicio14(){
+        int acelaracao = 2;      //  m/s²
+        int distancia = 500;     //  metros
+        int velocidadeMax = 12;  //  m/s
+        assertEquals(47,exe.tempoSemafaro(acelaracao,distancia,velocidadeMax));
+    }
+
+    @Test
+    void testeExercicio15(){
+        int sombraPredio = 150;
+        int sombraPessoa = 10;
+        int alturaPessoa = 180;
+        assertEquals(2700,exe.alturaPredio(sombraPredio,sombraPessoa,alturaPessoa));
+    }
+
+    @Test
+    void testeExercicio16(){
+        int numero = 498;
+        assertEquals(894,exe.inverterNumero(numero));
+    }
+
+    @Test
+    void testeExercicio17(){
+        int saque = 549;
+        assertEquals("(100:5,50:0,20:2,10:0,5:1,2:2,1:1)",exe.saqueOtimo(saque));
+    }
 
 }
