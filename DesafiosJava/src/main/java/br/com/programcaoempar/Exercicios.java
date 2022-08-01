1639d761b51e73a410b23795e5510135534d9193
Tiago <tiagocasa@gmail.com>
2022-08-01 00:17:52 -0300

    3 primeiros exercicios

--- DesafiosJava/src/main/java/br/com/programcaoempar/Exercicios.java
+++ DesafiosJava/src/main/java/br/com/programcaoempar/Exercicios.java
@@ -1,4 +1,25 @@
 package br.com.programcaoempar;
 
 public class Exercicios {
+    public Integer areaRetangulo(int base, int altura) {
+        return base*altura;
+    }
+    public Integer perimetroRetangulo(int base, int altura) {
+        return (base*2)+(altura*2);
+    }
+    public Integer areaQuadrado(int base, int altura) {
+        return base*altura;
+    }
+    public int perimetroQuadrado(int base, int altura) {
+        return (base*2)+(altura*2);
+    }
+
+
+    public double areaCirculo(int raio) {
+        return Math.PI*Math.pow(raio,2);
+    }
+
+    public double perimetroCirculo(int raio) {
+        return 2*Math.PI*raio;
+    }
 }
--- DesafiosJava/src/test/java/br/com/programcaoempar/Testes.java
+++ DesafiosJava/src/test/java/br/com/programcaoempar/Testes.java
@@ -1,4 +1,35 @@
 package br.com.programcaoempar;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Testes {
+
+    Exercicios exe;
+    @BeforeEach
+    void inicializacao(){ exe = new Exercicios();}
+@Test
+void testeExercicio1(){
+        int base = 4;
+        int altura = 5;
+    assertEquals(20,exe.areaRetangulo(base,altura));
+    assertEquals(18,exe.perimetroRetangulo(base,altura));
+}
+@Test
+void testeExercicio2(){
+    int base = 5;
+    int altura = 5;
+    assertEquals(20,exe.areaQuadrado(base,altura));
+    assertEquals(18,exe.perimetroQuadrado(base,altura));
+}
+
+@Test
+    void testeExercicio3(){
+        int raio = 2;
+        assertEquals(12.566370614359172,exe.areaCirculo(raio));
+        assertEquals(12.566370614359172,exe.perimetroCirculo(raio));
+}
+
 }
